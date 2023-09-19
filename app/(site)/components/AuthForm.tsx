19c2ebd59313d1e9f179461c3a4b66387de7f38d
Klip5a <62743514+Klip5a@users.noreply.github.com>
2023-09-19 17:31:58 +0500

    begin

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm,
+} from 'react-hook-form';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = data => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+      // Axios register
+    }
+
+    if (variant === 'LOGIN') {
+      // NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth Social SignIn
+  };
+
+  return (
+    <div
+      className='
+        mt-8
+        sm:mx-auto
+        sm:w-full
+        sm:max-w-md
+      '
+    >
+      <div
+        className='
+          bg-white
+          py-8
+          px-6
+          shadow
+          sm:rounded-lg
+          sm:px-10
+        '
+      >
+        <form></form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
