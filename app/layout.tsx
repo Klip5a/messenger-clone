d4fb1332b244f839e82dfa8e41d804625d1cfe4f
Klip5a <62743514+Klip5a@users.noreply.github.com>
2023-09-26 17:44:04 +0500

    practice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
+import ToasterContext from './context/ToasterContext';
+import AuthContext from './context/AuthContext';
+
 import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Messenger Clone',
+export const metadata = {
+  title: 'Messenger',
   description: 'Messenger Clone',
 };
 
@@ -16,7 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
     </html>
   );
 }
